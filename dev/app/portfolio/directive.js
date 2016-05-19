758f6afc593506f8effeac71dfd9c488f6873c9b
pnmougel <pnmougel@gmail.com>
2016-05-19 19:01:37 +0200

    Added images for the portfolio

--- dev/app/portfolio/directive.js
+++ dev/app/portfolio/directive.js
@@ -1,11 +1,13 @@
-App.directive('portfolio', function () {
+App.directive('portfolio', function ($http) {
     return {
         restrict: 'E',
         scope: {
 
         },
         link: function (scope) {
-
+            $http.get('data/projects.json').then(function(res){
+                scope.projects = res.data.projects;
+            });
         },
         templateUrl: 'app/portfolio/view.html'
     };
