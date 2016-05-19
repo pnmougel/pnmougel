df6470984ed0c4435b0da09944de4c7c84308c8b
pnmougel <pnmougel@gmail.com>
2016-05-19 19:02:25 +0200

    Removed a column in the contact

--- dev/app/contact/directive.js
+++ dev/app/contact/directive.js
@@ -4,6 +4,14 @@ App.directive('contact', function () {
         scope: {},
         link: function (scope) {
             scope.socialItems = [{
+                icon: 'fa-at',
+                label: 'pnmougel@gmail.com',
+                link: 'pnmougel@gmail.com'
+            },{
+                icon: 'fa-skype',
+                label: 'pnmougel',
+                link: ''
+            },{
                 icon: 'fa-linkedin',
                 label: 'LinkedIn',
                 link: 'https://www.linkedin.com/in/pierre-nicolas-mougel-3ba91848'
@@ -15,11 +23,12 @@ App.directive('contact', function () {
                 icon: 'fa-twitter',
                 label: 'Twitter',
                 link: 'https://twitter.com/pnmougel'
-            },{
-                icon: 'fa-github-alt',
-                label: 'Github',
-                link: ''
             }
+            //     ,{
+            //     icon: 'fa-github-alt',
+            //     label: 'Github',
+            //     link: ''
+            // }
             //     ,{
             //     icon: 'fa-stack-overflow',
             //     label: 'Stackoverflow',
